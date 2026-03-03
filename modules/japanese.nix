b9e82e4b13d978d10796df684fb9986db6c837cc
hogcycle <nate@gysli.ng>
2026-03-03 01:40:48 -0500

    japanese language support

--- modules/japanese.nix
+++ modules/japanese.nix
@@ -5,24 +5,10 @@
     fcitx5 = {
       waylandFrontend = true;
       addons = with pkgs; [
-        fcitx5-mozc
+        fcitx5-mozc-ut
         fcitx5-gtk
       ];
     };
   };
 
-  fonts.packages = with pkgs; [
-    ipafont
-    kochi-substitute
-    noto-fonts-cjk-sans
-  ];
-
-  # this was what was causing the problems ... once I commented it out, `Mozc` became available in the
-  # config tool, and my CAPS_LOCK+J binding started working
-  #
-  # environment.systemPackages = with pkgs; [
-  #   fcitx5
-  #   fcitx5-mozc
-  #   fcitx5-configtool
-  # ];
 }
--- modules/zsh.nix
+++ modules/zsh.nix
@@ -7,7 +7,13 @@
 	  repono = "kitty +kitten ssh -i ~/.ssh/homelab nate@192.168.1.140"; 
 	  udelgo = "kitty +kitten ssh -i ~/.ssh/udel-go ngysling@go.eecis.udel.edu"; 
 	  taildrop = "sudo tailscale file get .";
+	  rebuild = "sudo nixos-rebuild switch --flake /home/nate/nix-home";
     };
+	sessionVariables = { 
+      QT_IM_MODULE = "fcitx";
+      XMODIFIERS = "@im=fcitx";
+      SDL_IM_MODULE = "fcitx";
+	}; 
     initContent = "ssh-add ~/.ssh/github 2>/dev/null || true"; 
   }; 
   programs.starship = { 
