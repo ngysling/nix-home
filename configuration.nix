768ae6f6c6f7fa8510d30d268d6854ca7e2fec8c
hogcycle <nate@gysli.ng>
2026-03-03 19:20:42 -0500

    removed old override to patch tailscale

--- configuration.nix
+++ configuration.nix
@@ -5,20 +5,6 @@
 
 { config, lib, pkgs, ... }:
 {
-  nixpkgs.overlays = [
-  (_: prev: {
-  tailscale = prev.tailscale.overrideAttrs (old: {
-    checkFlags =
-      builtins.map (
-        flag:
-          if prev.lib.hasPrefix "-skip=" flag
-          then flag + "|^TestGetList$|^TestIgnoreLocallyBoundPorts$|^TestPoller$"
-          else flag
-      )
-      old.checkFlags;
-  });
-})
-]; 
   imports = [
       ./hardware-configuration.nix
   ];
