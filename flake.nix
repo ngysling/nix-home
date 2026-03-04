5784b12a5f662b0e8d8745e2ae60c7612480736a
hogcycle <nate@gysli.ng>
2026-03-03 19:21:08 -0500

    nixpkgs 25.05 -> 25.11

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
-    home-manager.url = "github:nix-community/home-manager/release-25.05"; 
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.11"; 
     home-manager.inputs.nixpkgs.follows = "nixpkgs"; 
 #    hyprland.url = "github:hyprwm/Hyprland"; 
   };
