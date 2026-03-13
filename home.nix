102d87f4ee92874127f1ea1248a9ea4679836bfe
hogcycle <nate@gysli.ng>
2026-03-12 21:08:22 -0400

    (chore): move hdm to a module

--- home.nix
+++ home.nix
@@ -1,27 +1,8 @@
 { inputs, pkgs, lib, ... }:
-
-let 
-    laptop = { 
-	  desc = "Chimei Innolux Corporation 0x1301";
-	  conf = "preferred,0x0,1.2";
-	};
-	sceptre = { 
-	  desc = "Sceptre Tech Inc Sceptre Z27"; 
-	  conf = "preferred,auto,1.5";
-	};
-  undocked-conf = pkgs.writeText "undocked" ''
-    monitor=desc:${laptop.desc},${laptop.conf} 
-  ''; 
-  
-  docked-conf = pkgs.writeText "docked" ''
-    monitor=desc:${sceptre.desc},${sceptre.conf}
-    monitor=desc:${laptop.desc},disable
-  '';
-in {
+{
   imports = [
     ./modules/hyprland.nix
     ./modules/zsh.nix
-    ./modules/kanshi.nix
     ./modules/waybar.nix
     ./modules/rofi.nix
     ./modules/kitty.nix
@@ -107,30 +88,5 @@ in {
   fonts.fontconfig.enable = true; 
 
   home.stateVersion = "25.05"; 
-    home.hyprdynamicmonitors = { 
-		enable = true;
-		installExamples = false;
-		systemdTarget = "graphical-session.target"; 
-		extraFlags = ["--disable-power-events"];
-		config = ''#toml
-		[profiles.docked]
-		config_file = "${docked-conf}"
-		config_file_type = "static"
-		[profiles.docked.conditions]
-	  
-		[[profiles.docked.conditions.required_monitors]]
-		description = "${laptop.desc}"
-	  
-		[[profiles.docked.conditions.required_monitors]]
-		description = "${sceptre.desc}"
-	  
-		[profiles.undocked]
-		config_file = "${undocked-conf}"
-		config_file_type = "static"
-		[profiles.undocked.conditions]
-		[[profiles.undocked.conditions.required_monitors]]
-		description = "${laptop.desc}"
-		''; 
-    }; 
 }
 
